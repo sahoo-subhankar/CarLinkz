ac22d67685212e7915e06cee63664c268d584639
Subhankar Sahoo <sahoosubha2020@gmail.com>
2023-08-11 10:02:32 +0530

    All Files Added

--- CarLinkz/settings.py
+++ CarLinkz/settings.py
@@ -1,5 +1,6 @@
 import os.path
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -9,10 +10,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h#crayh=w51&e07a^(@m-+dsm%teotn=rr$1z^g24ij2d__se$'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG", default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -82,11 +83,11 @@ WSGI_APPLICATION = 'carlinkz.wsgi.application'
 DATABASES = {
     'default': {   
         'ENGINE': 'django.db.backends.mysql',   
-        'NAME': 'carzone_db',   
-        'USER': 'root',   
-        'PASSWORD': 'password',   
-        'HOST': '127.0.0.1',   
-        'PORT': '3306',  
+        'NAME': config('DB_NAME'),   
+        'USER': config('DB_USER'),   
+        'PASSWORD': config('DB_PASSWORD'),   
+        'HOST': config('DB_HOST'),   
+        'PORT': config('DB_PORT'),  
     }
 }
 
@@ -145,12 +146,12 @@ SITE_ID = 1
 
 # Email Settings
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST_USER = "developer.external.email@gmail.com"
-EMAIL_HOST_PASSWORD = "trazkwaoxynlfnkn"
-EMAIL_USE_TLS = True
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
+EMAIL_PORT = config('EMAIL_PORT', cast=int)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
 
 
 # Default primary key field type
